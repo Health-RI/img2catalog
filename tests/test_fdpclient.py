801eda668e46e285132e8d5febccdc29119a79fe
Mark Janse <mark.janse@health-ri.nl>
2024-02-01 11:51:59 +0100

    Parametrized a test and cleanup

--- tests/test_fdpclient.py
+++ tests/test_fdpclient.py
@@ -6,9 +6,14 @@ import requests
 from xnatdcat.fdpclient import FDPClient
 
 
-# login:
-# Check if token gets correctly assigned
-# Check an exception gets raised for wrong credentials
+@pytest.fixture
+def fdp_client_mock(requests_mock):
+    requests_mock.post("http://fdp.example.com/tokens", json={"token": "1234abcd"})
+    fdp_client = FDPClient("http://fdp.example.com", "user@example.com", "pass")
+
+    return fdp_client
+
+
 def test_fdp_login(requests_mock):
     requests_mock.post("http://fdp.example.com/tokens", json={"token": "1234abcd"})
 
@@ -26,59 +31,52 @@ def test_fdp_login_error(requests_mock):
         FDPClient("http://fdp.example.com", "wrong_email", "wrong_password")
 
 
-def test_fdp_publish(requests_mock):
-    requests_mock.post("http://fdp.example.com/tokens", json={"token": "1234abcd"})
-
-    fdp_client = FDPClient("http://fdp.example.com", "user@example.com", "pass")
-
+def test_fdp_publish(requests_mock, fdp_client_mock):
     requests_mock.put(
         "https://fdp.example.com/dataset/12345678/meta/state",
     )
 
-    fdp_client.publish_record("https://fdp.example.com/dataset/12345678")
+    fdp_client_mock.publish_record("https://fdp.example.com/dataset/12345678")
 
     assert requests_mock.call_count == 2
     assert requests_mock.last_request.url == "https://fdp.example.com/dataset/12345678/meta/state"
     assert requests_mock.last_request.json() == {"current": "PUBLISHED"}
 
 
-def test_fdp_post_serialised(requests_mock):
-    requests_mock.post("http://fdp.example.com/tokens", json={"token": "1234abcd"})
-    requests_mock.post("http://fdp.example.com/dataset", text='')
-    fdp_client = FDPClient("http://fdp.example.com", "user@example.com", "pass")
+@pytest.mark.parametrize("metadata_type", ["dataset", "catalog", "distribution"])
+def test_fdp_post_serialised(requests_mock, fdp_client_mock, metadata_type):
+    requests_mock.post(f"http://fdp.example.com/{metadata_type}", text='')
 
     metadata = MagicMock(spec=Graph)
     metadata.serialize.return_value = ''
     # Ensure it's valid? Enforce lowercase?
-    fdp_client.post_serialized("dataset", metadata)
+    fdp_client_mock.post_serialized(metadata_type, metadata)
 
-    assert requests_mock.last_request.url == "http://fdp.example.com/dataset"
+    assert requests_mock.last_request.url == f"http://fdp.example.com/{metadata_type}"
     assert requests_mock.last_request.headers['Content-Type'] == 'text/turtle'
     assert requests_mock.last_request.headers['Authorization'] == 'Bearer 1234abcd'
     metadata.serialize.assert_called_once_with(format="turtle")
-    # pass
 
 
 # @pytest.mark.repeat(1)
 @patch("xnatdcat.fdpclient.FDPClient.post_serialized")
 @patch("xnatdcat.fdpclient.FDPClient.publish_record")
-def test_fdp_create_and_publish(publish_record, post_serialized, requests_mock):
-    requests_mock.post("http://fdp.example.com/tokens", json={"token": "1234abcd"})
+def test_fdp_create_and_publish(publish_record, post_serialized, requests_mock, fdp_client_mock):
     requests_mock.post("http://fdp.example.com/dataset", text='')
-    fdp_client = FDPClient("http://fdp.example.com", "user@example.com", "pass")
     empty_graph = Graph()
 
     # load the reference file and return it as post_response text with code 201 ('Created')
-    resp = requests.Response()
-    resp.status_code = 201
-    resp.headers = {'Location': "http://fdp.example.com/dataset/f1bcfd31-397e-4955-930c-663df8c2d9bf"}
+    # The FDP reference implementation returns the new identifier in HTTP location header
+    fdp_response = requests.Response()
+    fdp_response.status_code = 201
+    fdp_response.headers = {'Location': "http://fdp.example.com/dataset/f1bcfd31-397e-4955-930c-663df8c2d9bf"}
+    # requests expects a binary response content, not a string
     with open(file='tests/references/fdp_dataset.ttl', mode='rb') as f:
-        resp._content = f.read()
+        fdp_response._content = f.read()
 
-    # publish_record.side_effect = FDPClient.create_and_publish
-    post_serialized.return_value = resp
+    post_serialized.return_value = fdp_response
 
-    fdp_client.create_and_publish('dataset', empty_graph)
+    fdp_client_mock.create_and_publish('dataset', empty_graph)
 
     publish_record.assert_called_once_with(
         URIRef("http://fdp.example.com/dataset/f1bcfd31-397e-4955-930c-663df8c2d9bf")
