2948b93cc4b27de771c24463bb6a58afd62f44a3
Alexander Harms <alexander.harms@health-ri.nl>
2025-05-09 10:37:07 +0200

    fix(tests): integration tests for private projects

--- tests/test_xnat_integration_tests.py
+++ tests/test_xnat_integration_tests.py
@@ -8,6 +8,7 @@ from freezegun import freeze_time
 
 
 TEST_CONFIG = pathlib.Path(__file__).parent / "xnat_integration_test_config.toml"
+TEST_CONFIG_PRIVATE = pathlib.Path(__file__).parent / "xnat_integration_test_config_include_private.toml"
 
 @freeze_time("2024-04-01")
 @pytest.mark.integration
@@ -34,6 +35,37 @@ def test_xnat_integration(tmp_path, xnat4tests_connection, xnat4tests_uri, isola
     result_graph = empty_graph.parse(source=f"{tmp_path}/output.ttl")
     reference_graph = second_empty_graph.parse(
         source=pathlib.Path(__file__).parent / "references" / "xnat_integration_test.ttl")
+
+    # Verify known output
+    assert result.exit_code == 0
+    assert to_isomorphic(reference_graph) == to_isomorphic(result_graph)
+
+
+@freeze_time("2024-04-01")
+@pytest.mark.integration
+def test_xnat_integration_include_private(tmp_path, xnat4tests_connection, xnat4tests_uri, isolated_cli_runner,
+                                          empty_graph, second_empty_graph):
+    """ XNAT Integration test
+
+    Using xnat4tests, there is a local XNAT prepared with 5 projects:
+    - 1 public with the opt-out keyword,
+    - 1 public without any keywords,
+    - 1 private with optin keyword
+    - 1 public with optin keyword,
+    - 1 protected with optin keyword
+    There is also some metadata inserted; see xnat4tests_fixture() in xnatpy_fixtures.py.
+
+    Of these projects only the last three should be serialized.
+    """
+    # XNAT integration tests
+    result = isolated_cli_runner.invoke(cli_click, ["--verbose", "--config", f"{TEST_CONFIG_PRIVATE}",
+                                                    "xnat", "--server", xnat4tests_uri, "-u", "admin", "-p", "admin",
+                                                    "map-xnat-hriv2",
+                                                    "rdf", "-o", f"{tmp_path}/output.ttl"])
+    print(result.stdout)
+    result_graph = empty_graph.parse(source=f"{tmp_path}/output.ttl")
+    reference_graph = second_empty_graph.parse(
+        source=pathlib.Path(__file__).parent / "references" / "xnat_integration_test_include_private.ttl")
 
     # Verify known output
     assert result.exit_code == 0
