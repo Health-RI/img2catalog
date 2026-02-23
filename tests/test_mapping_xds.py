8afeab4256ae72725201b23dc31814d3212df818
SeanBerrieHRI <sean.berrie@health-ri.nl>
2026-02-23 11:20:45 +0100

    test: add base for mapping_xds tests

---
+++ tests/test_mapping_xds.py
@@ -0,0 +1,17 @@
+def test_format_temporal_coverage_raises_value_error():
+    return
+
+def test_format_temporal_coverage_returns_valid_periodoftime():
+    return
+
+def test_format_title_returns_formatted_string():
+    return
+
+def test_format_title_raises_error_on_missing_field():
+    return
+
+def test_map_xds_to_healthri_dcat_dataset_returns_model():
+    return
+
+def test_map_xds_to_healthri_dcat_dataset_raises_on_missing_field ():
+    return
