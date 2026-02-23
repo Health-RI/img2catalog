03b89f85a621cfc035b4918f101d14626b16a3e1
SeanBerrieHRI <sean.berrie@health-ri.nl>
2026-02-23 10:22:33 +0100

    feature: add title and temporal coverage formatters

--- src/img2catalog/mappings/xds.py
+++ src/img2catalog/mappings/xds.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 from pathlib import Path
 from typing import Dict
+
+from sempyro.time import PeriodOfTime
 
 from img2catalog.configmanager import load_img2catalog_configuration
 from pandas import Series
@@ -8,12 +11,38 @@ from sempyro.hri_dcat import HRIAgent, HRIVCard, HRIDataset
 
 from img2catalog.inputs.csv import read_csv
 
+def format_temporal_coverage(temporal_coverage: str) -> PeriodOfTime:
+    """Format 'DD-MM-YYYY to DD-MM-YYYY' into a PeriodOfTime object."""
+    start_str, end_str = temporal_coverage.split("to")
+
+    date_fmt = "%d-%m-%Y"
+    start_date = datetime.strptime(start_str.strip(), date_fmt)
+    end_date = datetime.strptime(end_str.strip(), date_fmt)
+
+    return PeriodOfTime(
+        start_date=start_date.strftime(date_fmt),
+        end_date=end_date.strftime(date_fmt)
+    )
+
+def format_title(data) -> str:
+    """ Format dataset title with instituteName, modality, start- and end date requirements."""
+    try:
+        modality = data["modality"]
+        institute = data["instituteName"]
+        period = format_temporal_coverage(data["temporalCoverage"])
+    except KeyError as e:
+        raise KeyError(f"Missing required field in data: {e}")
+
+    return f"{institute}_{modality}_{period.start_date.value}_{period.end_date.value}"
 
 def map_xds_to_healthri_dcat_dataset(rows: Series, config: Dict) -> HRIDataset:
     _, data = rows
     agentConfig = config.get("agent")
     vCardConfig = config.get("v_card")
     datasetConfig = config.get("dataset")
+
+    # formatting
+    dataset_formatted_title = format_title(data)
 
     agent = HRIAgent(
         name=[{"value": data["instituteName"], "lang": "en"}],
@@ -30,7 +59,7 @@ def map_xds_to_healthri_dcat_dataset(rows: Series, config: Dict) -> HRIDataset:
     dataset = HRIDataset(
         # MANDATORY DCAT FIELDS
         identifier = datasetConfig["identifier"],
-        title=[{"value": datasetConfig["title"], "lang": "en"}],
+        title=[{"value": dataset_formatted_title, "language": "en"}],
         description=[{"value": datasetConfig["description"], "lang": "en"}],
         publisher = agent,
         contact_point = vCard,
@@ -51,7 +80,6 @@ def map_xds_to_healthri_dcat_dataset(rows: Series, config: Dict) -> HRIDataset:
 
 if __name__ == '__main__':
     dataframe = read_csv("../inputs/xds_input.csv")
-
     config_path = Path("../../../examples/xds_example_config.toml")
     config = load_img2catalog_configuration(config_path)
 
@@ -59,4 +87,4 @@ if __name__ == '__main__':
     for row in dataframe.iterrows():
         dataset = map_xds_to_healthri_dcat_dataset(row, config)
         datasets.append(dataset)
-        print(dataset)
+        print(dataset.title)
