1443011c785e5895deac7acacdc79875f2ae8283
SeanBerrieHRI <sean.berrie@health-ri.nl>
2026-02-23 11:20:08 +0100

    test: add input_csv test

---
+++ tests/test_input_csv.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from img2catalog.inputs.csv import read_csv
+
+def test_read_csv_returns_dataframe_on_success():
+    # Arrange
+    filepath = "examples/xds_input.csv"
+    expected_results = 4
+
+    # Act
+    result = read_csv(filepath)
+
+    # Assert
+    assert not result.empty
+
+def test_read_csv_returns_empty_dataframe_on_error():
+    # Arrange
+    invalid_path = "non_existent_file.csv"
+
+    # Act
+    result = read_csv(invalid_path)
+
+    # Assert
+    assert isinstance(result, pd.DataFrame)
+    assert result.empty
