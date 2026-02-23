01b50605193d1ea3e7b2efc3e089a1aeb64020cf
SeanBerrieHRI <sean.berrie@health-ri.nl>
2026-02-23 11:36:53 +0100

    test: add tests for temporal coverage

--- tests/test_mapping_xds.py
+++ tests/test_mapping_xds.py
@@ -1,8 +1,28 @@
+import pytest
+from sempyro.time import PeriodOfTime
+
+from img2catalog.mappings.xds import format_temporal_coverage
+
+
 def test_format_temporal_coverage_raises_value_error():
-    return
+    # Arrange
+    invalid_temporal_coverage = "01-01-2026_31-12-2026"
+
+    # Act & Assert
+    with pytest.raises(ValueError):
+        format_temporal_coverage(invalid_temporal_coverage)
 
 def test_format_temporal_coverage_returns_valid_periodoftime():
-    return
+    # Arrange
+    temporal_coverage = "01-01-2026 to 31-12-2026"
+
+    # Act
+    result = format_temporal_coverage(temporal_coverage)
+
+    # Assert
+    assert isinstance(result, PeriodOfTime)
+    assert result.start_date == "01-01-2026"
+    assert result.end_date == "31-12-2026"
 
 def test_format_title_returns_formatted_string():
     return
