e9958c78e792d3ec64b878b66e9c989bb3c1d5b3
SeanBerrieHRI <sean.berrie@health-ri.nl>
2026-03-06 10:42:11 +0100

    refactor: change Dataframe import and remove __main__

--- src/img2catalog/inputs/csv_reader.py
+++ src/img2catalog/inputs/csv_reader.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from pandas.core.interchange.dataframe_protocol import DataFrame
+from pandas import DataFrame
+
 
 def read_csv(csv_path: str) -> DataFrame:
     try:
@@ -7,11 +8,3 @@ def read_csv(csv_path: str) -> DataFrame:
     except Exception as e:
         print(f"error reading .csv file: {e}")
         return pd.DataFrame()
-
-if __name__ == '__main__':
-    results = read_csv("../../../examples/xds_input.csv")
-
-    if not results.empty:
-        print(results.to_json(orient='records', indent=2))
-    else:
-        print("No data found or file is empty.")
